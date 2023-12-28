c85dbe272758ac46598b1bb4f1b2a471c92a9527
anxiousss <145536203+anxiousss@users.noreply.github.com>
2023-12-28 20:00:26 +0300

    Update G7_Same_type.py

--- 4/4.3/G7_Same_type.py
+++ 4/4.3/G7_Same_type.py
@@ -15,8 +15,3 @@ def same_type(f):
 @same_type
 def combine_text(*words):
     return ' '.join(words)
-
-
-print(combine_text('Hello,', 'world!') or 'Fail')
-print(combine_text(2, '+', 2, '=', 4) or 'Fail')
-print(combine_text('Список из 30', 0, 'можно получить так', [0] * 30) or 'Fail')
