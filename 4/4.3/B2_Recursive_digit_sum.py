e7c6bfe47ddbbf4913264162ece75d00798cf440
anxiousss <145536203+anxiousss@users.noreply.github.com>
2023-12-28 19:59:02 +0300

    Update B2_Recursive_digit_sum.py

--- 4/4.3/B2_Recursive_digit_sum.py
+++ 4/4.3/B2_Recursive_digit_sum.py
@@ -6,7 +6,3 @@ def recursive_digit_sum(number):
     last_digit = number % 10
     number //= 10
     return last_digit + recursive_digit_sum(number)
-
-
-result = recursive_digit_sum(7321346)
-print(result)
