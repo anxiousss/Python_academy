b3136539b6a09a770b57558daa7f07fc7a0b260e
anxiousss <145536203+anxiousss@users.noreply.github.com>
2024-03-13 00:20:39 +0300

    Update C3_Red_button.py

--- 5/5.1/C3_Red_button.py
+++ 5/5.1/C3_Red_button.py
@@ -10,13 +10,3 @@ class RedButton:
 
     def count(self):
         return self.n_req
-
-
-first_button = RedButton()
-second_button = RedButton()
-for time in range(5):
-    if time % 2 == 0:
-        second_button.click()
-    else:
-        first_button.click()
-print(first_button.count(), second_button.count())
